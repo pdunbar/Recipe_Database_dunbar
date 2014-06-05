2e73f12a9c34afa5a74b3e3877380b311a0aebe6
Peter Dunbar <Peter.joseph.dunbar@gmail.com>
2014-06-05 19:06:14 -0400

    cleaned up a little bit. removed some reduntant code. time to dive into design...

--- server.rb
+++ server.rb
@@ -1,22 +1,18 @@
 require_relative 'server_methods'
 require 'sinatra'
 
-
 get '/' do
   redirect '/recipes'
 end
-
 
 get '/recipes' do
   @recipes = get_recipes
   erb :recipes
 end
 
-
 get '/recipes/:id' do
   @recipe_id = params[:id]
   @recipe, @ingredients = get_recipe(@recipe_id)
-
   erb :recipe
 end
 
--- server_methods.rb
+++ server_methods.rb
@@ -1,12 +1,9 @@
 require 'pg'
 
-
 def db_connection
   begin
     connection = PG.connect(dbname: 'recipes')
-
     yield(connection)
-
   ensure
     connection.close
   end
@@ -16,18 +13,15 @@ def get_recipes()
   query = "SELECT recipes.id as recipe_id, recipes.name as name
            FROM recipes
            WHERE recipes.description IS NOT NULL"
-
   recipes = db_connection do |conn|
     conn.exec(query)
   end
 
-  recipes = recipes.to_a
   recipes = recipes.sort_by do |recipe|
     recipe["name"]
   end
   return recipes
 end
-
 
 def get_recipe(recipe_id)
   query = " SELECT recipes.id, recipes.name as name,
@@ -40,13 +34,9 @@ def get_recipe(recipe_id)
 
   query = "SELECT ingredients.name as ingredient
            FROM recipes
-           JOIN ingredients on ingredients.recipe_id = recipes.id
            WHERE recipes.id = $1"
-
   ingredients = db_connection do |conn|
     conn.exec_params(query, [recipe_id])
   end
-  recipe = recipe.to_a
-  ingredients = ingredients.to_a
   return recipe, ingredients
 end
